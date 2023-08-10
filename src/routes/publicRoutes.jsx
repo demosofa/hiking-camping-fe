ca825937e006c5ad024e57db02b70d6245b27c2f
VanThen60hz <nguyenvthang2409@gmail.com>
2023-08-10 14:14:42 +0700

    feat: shop

--- src/routes/publicRoutes.jsx
+++ src/routes/publicRoutes.jsx
@@ -18,16 +18,6 @@ const publicRoutes = [
 		path: '/collection/all',
 		element: <Shop />,
 	},
-
-	{
-		path: '/collection/all',
-		element: <Shop />,
-	},
-
-	{
-		path: '/collection/all',
-		element: <Shop />,
-	},
 ];
 
 export default publicRoutes;
