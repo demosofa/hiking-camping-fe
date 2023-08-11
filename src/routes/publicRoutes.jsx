5bbd7f0faebdae9f5510d3af9ecb084980a72b56
VanThen60hz <nguyenvthang2409@gmail.com>
2023-08-11 13:29:44 +0700

    Feat/shop (#11)
    * feat: shop
    
    * feat: shop
    
    * feat: shop
    
    * Feat/shop (#8)
    
    * feat: shop
    
    * feat: shop
    
    * feat: shop

--- src/routes/publicRoutes.jsx
+++ src/routes/publicRoutes.jsx
@@ -1,5 +1,6 @@
 import Footer from '@layout/Footer/Footer';
 import HikingPage from 'pages/Hiking/HikingPage';
+import { Shop } from 'pages/Shop/Shop';
 
 /**@type {import("react-router-dom").RouteObject[]} */
 const publicRoutes = [
@@ -7,9 +8,15 @@ const publicRoutes = [
 		path: '/hehe',
 		element: <Footer />,
 	},
+
 	{
 		path: '/hoho',
 		element: <HikingPage />,
+	},
+
+	{
+		path: '/collection/all',
+		element: <Shop />,
 	},
 ];
 
