dcdd2d8324188e01d0c6a1b1c515b5501cafee28
VanThen60hz <nguyenvthang2409@gmail.com>
2023-08-11 12:48:04 +0700

    Feat/shop (#9)
    * feat: shop
    
    * feat: shop
    
    * feat: shop
    
    * feat: shop
    
    * fix: add antd icon
    
    * fix: fix commit
    
    * Feat/feat 03 (#7)
    
    * feat: first commit
    
    * feat: hiking ui
    
    * feat: shop
    
    * feat: shop
    
    * Feat/shop (#8)
    
    * feat: shop
    
    * feat: shop
    
    ---------
    
    Co-authored-by: datnvn <idolsupperman3215@gmail.com>
    Co-authored-by: NguyenThanhNhan2507 <65636315+NguyenThanhNhan2507@users.noreply.github.com>

--- src/routes/publicRoutes.jsx
+++ src/routes/publicRoutes.jsx
@@ -1,5 +1,4 @@
 import Footer from '@layout/Footer/Footer';
-import HikingPage from 'pages/Hiking/HikingPage';
 import { Shop } from 'pages/Shop/Shop';
 
 /**@type {import("react-router-dom").RouteObject[]} */
@@ -10,8 +9,13 @@ const publicRoutes = [
 	},
 
 	{
-		path: '/hoho',
-		element: <HikingPage />,
+		path: '/collection/all',
+		element: <Shop />,
+	},
+
+	{
+		path: '/collection/all',
+		element: <Shop />,
 	},
 
 	{
