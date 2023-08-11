8bcd7eb8031e33413191b6f971ad57dc1c4399b1
NguyenThanhNhan2507 <65636315+NguyenThanhNhan2507@users.noreply.github.com>
2023-08-11 11:17:14 +0700

    Feat/feat 03 (#7)
    * feat: first commit
    
    * feat: hiking ui

--- src/routes/publicRoutes.jsx
+++ src/routes/publicRoutes.jsx
@@ -1,10 +1,15 @@
 import Footer from '@layout/Footer/Footer';
+import HikingPage from 'pages/Hiking/HikingPage';
 
 /**@type {import("react-router-dom").RouteObject[]} */
 const publicRoutes = [
 	{
 		path: '/hehe',
 		element: <Footer />,
+	},
+	{
+		path: '/hoho',
+		element: <HikingPage />,
 	},
 ];
 
