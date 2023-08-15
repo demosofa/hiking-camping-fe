1884d9d271338151a141da6b6d3e614f06338466
demosofa <62540039+demosofa@users.noreply.github.com>
2023-08-15 22:29:58 +0700

    feat: init header with simple navbar (#20)

---
+++ src/components/MenuNav/MenuNav.jsx
@@ -0,0 +1,269 @@
+import { Menu } from 'antd';
+import { memo } from 'react';
+import './menunav.css';
+import { Link } from 'react-router-dom';
+
+function MenuNav({ mode = 'horizontal' }) {
+	return (
+		<Menu
+			className="navbar"
+			mode={mode}
+			items={[
+				{
+					label: 'Shop',
+					className: 'nav-item',
+					popupClassName: 'group-nav',
+					children: [
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+					],
+				},
+				{
+					label: 'Hiking',
+					className: 'nav-item',
+					popupClassName: 'group-nav',
+					children: [
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+					],
+				},
+				{
+					label: 'Trekking',
+					className: 'nav-item',
+					popupClassName: 'group-nav',
+					children: [
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+					],
+				},
+				{
+					label: 'Camping',
+					className: 'nav-item',
+					popupClassName: 'group-nav',
+					children: [
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+						{
+							type: 'group',
+							label: (
+								<Link className="group-title" to={`/`}>
+									free climbing
+								</Link>
+							),
+							children: [
+								{
+									label: 'Camping',
+								},
+							],
+							style: { flex: 1 },
+						},
+					],
+				},
+			]}
+		/>
+	);
+}
+
+export default memo(MenuNav);
