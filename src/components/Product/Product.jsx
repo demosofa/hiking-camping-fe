2ff3cf72a730968d664db682ecc2bfacb3596f0e
VanThen60hz <nguyenvthang2409@gmail.com>
2023-08-16 15:03:24 +0700

    Feat/shop (#32)
    * feat: shop
    
    * feat: shop
    
    * feat: shop
    
    * Feat/shop (#8)
    
    * feat: shop
    
    * feat: shop
    
    * feat: shop
    
    * fix: paralax
    
    * feat: shop
    
    * feat: shop
    
    * Feat/shop (#8)
    
    * feat: shop
    
    * feat: shop
    
    * feat: shop
    
    * feat: home
    
    * feat: shop add product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,15 @@
-import { Col, Row, Card } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
-import { Dropdown, Space } from 'antd';
-import { useGetProduct } from 'hooks/useGetProduct';
+import {
+	AppstoreFilled,
+	CaretDownOutlined,
+	CaretUpOutlined,
+	UnorderedListOutlined,
+} from '@ant-design/icons';
+import { Card, Col, Dropdown, Row, Space } from 'antd';
 import Rating from 'components/Rating/Rating';
+import { useGetProduct } from 'hooks/useGetProduct';
 import { Link } from 'react-router-dom';
+import './Product.css';
+import { useState } from 'react';
 
 export const Product = () => {
 	const items = [
@@ -60,128 +66,148 @@ export const Product = () => {
 		},
 	];
 
+	const [expanded, setExpanded] = useState(false);
+
+	const toggleExpand = () => {
+		setExpanded(!expanded);
+	};
+
 	const { data: products, isLoading: loadingProduct } = useGetProduct();
 
 	return (
 		<>
-			<Col className="dropdown-content" span={16}>
-				<Row className="dropdown" align={'middle'}>
-					<Col span={7}></Col>
-					<Col>
-						<Row>
-							<p className="text-dropdown">Sort By</p>
-							<Dropdown
-								menu={{
-									items,
-								}}
-								trigger={['click']}
-							>
-								<a onClick={(e) => e.preventDefault()}>
-									<Space className="dropdown-space">
-										FEATURED
-										<DownOutlined className="dropdown-icon" />
-									</Space>
-								</a>
-							</Dropdown>
-						</Row>
-					</Col>
-					<Col className="Bienmat" span={2}></Col>
-					<Col>
-						<Row>
-							<p className="text-dropdown">Show</p>
-							<Dropdown menu={{ items: shows }} trigger={['click']}>
-								<a onClick={(e) => e.preventDefault()}>
-									<Space
-										style={{
-											width: '140px',
-											height: '41px',
-											background: 'white',
-											border: '1px solid #e4e4e4',
-											display: 'flex',
-											justifyContent: 'space-evenly',
-										}}
-									>
-										12
-										<DownOutlined />
-									</Space>
-								</a>
-							</Dropdown>
-						</Row>
-					</Col>
-				</Row>
-				<Row justify="center">
-					{loadingProduct ? null : products && products.length > 0 ? (
-						products.map((product) =>
-							product.variant ? (
-								<Col
-									key={product.id}
-									xs={24}
-									md={11}
-									lg={7}
-									xl={8}
-									className="row-wrap"
+			<Row className="dropdown" align={'middle'}>
+				<Col span={7}>
+					<div style={{ display: 'flex' }}>
+						<a href="" title="Grid view" className="grid-view">
+							<AppstoreFilled className="view-icon" />
+						</a>
+						<a href="" title="List view" className="list-view">
+							<UnorderedListOutlined className="view-icon" />
+						</a>
+					</div>
+				</Col>
+				<Col>
+					<Row>
+						<p className="text-dropdown">Sort By</p>
+						<Dropdown
+							menu={{
+								items,
+							}}
+							trigger={['click']}
+						>
+							<a onClick={(e) => e.preventDefault()}>
+								<Space className="dropdown-space">
+									<p></p>
+									<p></p>
+									<p></p>FEATURED
+									<span onClick={toggleExpand}>
+										{expanded ? (
+											<CaretUpOutlined className="dropdown-icon" />
+										) : (
+											<CaretDownOutlined className="dropdown-icon" />
+										)}
+									</span>
+									<p></p>
+								</Space>
+							</a>
+						</Dropdown>
+					</Row>
+				</Col>
+				<Col span={2}></Col>
+				<Col>
+					<Row>
+						<p className="text-dropdown">Show</p>
+						<Dropdown menu={{ items: shows }} trigger={['click']}>
+							<a onClick={(e) => e.preventDefault()}>
+								<Space
+									style={{
+										width: '140px',
+										height: '41px',
+										background: 'white',
+										border: '1px solid #e4e4e4',
+										display: 'flex',
+										justifyContent: 'space-evenly',
+									}}
 								>
-									<a href={`/collections/${product.id}`}>
-										<Card
-											hoverable
-											className="card-wrap"
-											style={{ padding: '0' }}
-											cover={
-												<>
-													<div
-														style={{
-															display: 'flex',
-															justifyContent: 'center',
-															// height: '300px',
-														}}
-													>
-														<img
-															src={`http://localhost:3000/${product.variant[0].image}`}
-															className="product-img"
-															alt="Hiking"
-															crossOrigin="anonymous"
-														/>
-													</div>
-													<div className="grid-link__title">
-														<a className="product-text-name">{product.name}</a>
-														<Link className="grid-color">
-															{product.variant.map((variant) => (
-																<Link
-																	key={product.id}
-																	className="grid-image"
-																	style={{
-																		background: `${variant.color.color}`,
-																	}}
-																></Link>
-															))}
-														</Link>
-													</div>
+									12
+									<CaretDownOutlined />
+								</Space>
+							</a>
+						</Dropdown>
+					</Row>
+				</Col>
+			</Row>
+			<Row justify="center">
+				{loadingProduct ? null : products && products.length > 0 ? (
+					products.map((product) =>
+						product.variant ? (
+							<Col
+								key={product.id}
+								xs={24}
+								md={11}
+								lg={7}
+								xl={8}
+								className="row-wrap"
+							>
+								<a href={`/product/overview/${product.id}`}>
+									<Card
+										hoverable
+										className="card-wrap"
+										style={{ padding: '0' }}
+										cover={
+											<>
+												<div
+													style={{
+														display: 'flex',
+														justifyContent: 'center',
+														// height: '300px',
+													}}
+												>
+													<img
+														src={`http://localhost:3000/${product.variant[0].image}`}
+														className="product-img"
+														alt="Hiking"
+														crossOrigin="anonymous"
+													/>
+												</div>
+												<div className="grid-link__title">
+													<a className="product-text-name">{product.name}</a>
+													<Link className="grid-color">
+														{product.variant.map((variant) => (
+															<Link
+																key={product.id}
+																className="grid-image"
+																style={{
+																	background: `${variant.color.color}`,
+																}}
+															></Link>
+														))}
+													</Link>
+												</div>
 
-													<div className="product-price-name">
-														<span className="">
-															${product.variant[0].price}
-														</span>
-														<div className="product-rating-pro">
-															<Rating rating={product.review[0]?.rating} />
-														</div>
+												<div className="product-price-name">
+													<span className="">${product.variant[0].price}</span>
+													<div className="product-rating-pro">
+														<Rating rating={product.review[0]?.rating} />
 													</div>
-												</>
-											}
-										/>
-									</a>
-								</Col>
-							) : null
-						)
-					) : (
-						<p>No categories found.</p>
-					)}
-					{/* {
+												</div>
+											</>
+										}
+									/>
+								</a>
+							</Col>
+						) : null
+					)
+				) : (
+					<p>No categories found.</p>
+				)}
+				{/* {
 								loadingProduct ? null : products && products.length > 0 ? 
 								(products.map((product) => product.variant ? (<Col></Col>): null)):(<p>hehehe</p>
 								)
 							} */}
-				</Row>
-			</Col>
+			</Row>
 		</>
 	);
 };
--- src/routes/publicRoutes.jsx
+++ src/routes/publicRoutes.jsx
@@ -31,7 +31,7 @@ const publicRoutes = [
 			},
 
 			{
-				path: 'product/overview',
+				path: 'product/overview/:id',
 				element: <ProductOverview />,
 			},
 
@@ -56,6 +56,7 @@ const publicRoutes = [
 		path: 'product/overview',
 		element: <ProductOverview />,
 	},
+
 	{
 		path: '/',
 		element: <CartItem />,
@@ -88,6 +89,11 @@ const publicRoutes = [
 	{
 		path: '/hihi',
 		element: <ProductPage />,
+	},
+
+	{
+		path: '/collection/all',
+		element: <Shop />,
 	},
 ];
 
