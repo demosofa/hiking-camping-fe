c1e7266009e0a6d297fcd65947ec5fabd77ebaa1
demosofa <thientndgcd18746@fpt.edu.vn>
2023-08-17 00:01:48 +0700

    refactor: auth

--- src/components/PrivatePage.jsx
+++ src/components/PrivatePage.jsx
@@ -2,7 +2,7 @@ import { useAuth } from '@hooks';
 import { Navigate } from 'react-router-dom';
 
 export function PrivatePage({ children, roles }) {
-	const { role } = useAuth();
+	const { role } = useAuth()();
 	const checkRole = roles.some((item) => item === role);
 
 	if (checkRole) return children;
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import privateRoutes from 'routes/privateRoutes';
 import publicRoutes from 'routes/publicRoutes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from '@contexts';
 
 const queryClient = new QueryClient();
 const router = createBrowserRouter([...privateRoutes, ...publicRoutes]);
@@ -15,8 +16,10 @@ axios.defaults.baseURL = BASE_URL;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
-		</QueryClientProvider>
+		<AuthProvider>
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
+		</AuthProvider>
 	</React.StrictMode>
 );
