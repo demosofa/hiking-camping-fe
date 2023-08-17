7410d94b6879b508e0347dfd44d1fd8d8952d522
Nguyen Thanh Nhan <nhandaica125@gmail.com>
2023-08-17 11:24:17 +0700

    fix: fix hiking

--- src/services/getCategory.js
+++ src/services/getCategory.js
@@ -1,10 +1,4 @@
 import axios from 'axios';
 import { API_URL } from '../constants/url';
 
-export const getCategory = () =>
-	axios.get(API_URL.CATEGORY, {
-		headers: {
-			Authorization:
-				'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImRmOWRlMWNiLTcyOGEtNGI0OS04ZjAzLWJjY2NmNGIxOTA0MCIsImZ1bGxOYW1lIjoibmhhbiBuZ3V5ZW4iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2OTIwNzA4NzN9.dOAjdVq5JrH4GVOWSBUkQ3KuKP2aUTi360BlBqfAFyU',
-		},
-	});
+export const getCategory = () => axios.get(API_URL.CATEGORY);
