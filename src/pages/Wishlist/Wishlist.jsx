e2e1003ac45ab3eb72d1fde31859fd68a6b0d54a
Đạt <93239546+C1021G1datnvn@users.noreply.github.com>
2023-08-17 09:06:40 +0700

    Feat/wishlist (#41)
    * feat: trekking
    
    * fix: fix
    
    * feat: wishlist

---
+++ src/pages/Wishlist/Wishlist.jsx
@@ -0,0 +1,134 @@
+import { Parallax } from 'components/Parallax';
+import './Wishlist.css';
+import { Button, Space, Table } from 'antd';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+axios.defaults.headers['Authorization'] =
+	'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijk2ZDBkMWVkLTY2ZGItNDFjMi05MmE0LTdlNjE0ZmYyYmQ0ZSIsImZ1bGxOYW1lIjoibmd1eWVuIHZhbiB0aMOpbmcgYsO5bG9uZyIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY5MjE2MTEzOX0.8IdfswpYahC7X4BcU93ZobmLtDkecIgh2ShHJhLWrl4';
+
+export const Wishlist = () => {
+	const [wishlist, setWishlist] = useState([]);
+	// const [deleted, setDeleted] = useState([]);
+	useEffect(() => {
+		axios
+			.get('http://localhost:3000/wishlist')
+			.then((response) => {
+				setWishlist(response.data.products);
+			})
+			.catch((error) => {
+				console.log('Error fetching cart data:', error);
+			});
+	}, []);
+
+	const handleDelete = (id) => {
+		axios
+			.delete(`http://localhost:3000/wishlist/${id}`)
+			.then(() => {
+				setWishlist((prev) => prev.filter((item) => item.id !== id));
+			})
+			.catch((error) => {
+				console.error('Error deleting item:', error);
+			});
+	};
+
+	const columns = [
+		{
+			title: '___',
+			key: 'delete',
+			render: (_, record) => (
+				<Space size="middle">
+					<a onClick={() => handleDelete(record.id)}>x</a>
+				</Space>
+			),
+		},
+		{
+			title: '___',
+			dataIndex: 'variant',
+			key: 'image',
+			render: (variants) => (
+				<span>
+					{variants.map((variant) => (
+						<span key={variant.id}>
+							<img
+								style={{ maxWidth: 200 }}
+								src={'http://localhost:3000/' + variant.image}
+								crossOrigin="anonymous"
+							/>
+							<br />
+						</span>
+					))}
+				</span>
+			),
+		},
+		{
+			title: 'Product',
+			dataIndex: 'name',
+			key: 'name',
+			render: (name) => <span>{name}</span>,
+		},
+		{
+			title: 'Price',
+			dataIndex: 'variant',
+			key: 'price',
+			render: (variants) => (
+				<span>
+					{variants.map((variant) => (
+						<span key={variant.id}>
+							{variant.price}
+							<br />
+						</span>
+					))}
+				</span>
+			),
+		},
+		{
+			title: 'Stock status',
+			dataIndex: 'variant',
+			key: 'stock',
+			render: (variants) => (
+				<span>
+					{variants.map((variant) => (
+						<span key={variant.id}>
+							{variant.stock}
+							<br />
+						</span>
+					))}
+				</span>
+			),
+		},
+		{
+			title: '___',
+			key: 'review',
+			render: () => (
+				<Space className="action-btn" size="middle">
+					<Link>
+						<Button className="btn-handle">VIEW PRODUCT</Button>
+					</Link>
+				</Space>
+			),
+		},
+	];
+
+	const ImageTable = ({ items }) => {
+		return (
+			<Table
+				className="custom-table"
+				dataSource={items}
+				columns={columns}
+				rowKey="id"
+			/>
+		);
+	};
+	return (
+		<div className="cart_body">
+			<div className="bg-parallax">
+				<Parallax pageTitle={'Wishlist'} />
+			</div>
+			<div>
+				<ImageTable items={wishlist} />
+			</div>
+		</div>
+	);
+};
